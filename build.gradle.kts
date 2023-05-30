286e393d75a34e99f32f997a6fa0162e2381558e
Bactrosaurus <aubererdaniel@tutanota.de>
2023-05-31 01:16:12 +0200

    Updated dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
-    kotlin("jvm") version "1.8.20"
-    kotlin("plugin.serialization") version "1.8.20"
-    id("fabric-loom") version "1.1-SNAPSHOT"
+    kotlin("jvm") version "1.8.21"
+    kotlin("plugin.serialization") version "1.8.21"
+    id("fabric-loom") version "1.2-SNAPSHOT"
 }
 
 group = "de.daniel"
@@ -15,17 +15,17 @@ repositories {
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.0")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.1")
 
     minecraft("com.mojang:minecraft:1.19.4")
     mappings(loom.officialMojangMappings())
 
-    modImplementation("net.fabricmc:fabric-loader:0.14.19")
-    modImplementation("net.fabricmc.fabric-api:fabric-api:0.78.0+1.19.4")
-    modImplementation("net.fabricmc:fabric-language-kotlin:1.9.3+kotlin.1.8.20")
+    modImplementation("net.fabricmc:fabric-loader:0.14.21")
+    modImplementation("net.fabricmc.fabric-api:fabric-api:0.82.0+1.19.4")
+    modImplementation("net.fabricmc:fabric-language-kotlin:1.9.4+kotlin.1.8.21")
 
-    modApi("com.terraformersmc:modmenu:6.2.0")
+    modApi("com.terraformersmc:modmenu:6.2.2")
     modApi("me.shedaniel.cloth:cloth-config-fabric:10.0.96") {
         exclude(group = "net.fabricmc.fabric-api")
     }
