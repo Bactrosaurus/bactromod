9110b1981d6d4dab3468c50d4277cc7f2925fda5
Bactrosaurus <aubererdaniel@tutanota.de>
2023-07-09 00:00:09 +0200

    Updated dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
-    kotlin("jvm") version "1.8.22"
-    kotlin("plugin.serialization") version "1.8.22"
-    id("fabric-loom") version "1.2-SNAPSHOT"
+    kotlin("jvm") version "1.9.0"
+    kotlin("plugin.serialization") version "1.9.0"
+    id("fabric-loom") version "1.3-SNAPSHOT"
 }
 
 group = "de.daniel"
@@ -16,14 +16,14 @@ repositories {
 
 dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.1")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.2")
 
     minecraft("com.mojang:minecraft:1.20.1")
     mappings(loom.officialMojangMappings())
 
     modImplementation("net.fabricmc:fabric-loader:0.14.21")
-    modImplementation("net.fabricmc.fabric-api:fabric-api:0.83.1+1.20.1")
-    modImplementation("net.fabricmc:fabric-language-kotlin:1.9.5+kotlin.1.8.22")
+    modImplementation("net.fabricmc.fabric-api:fabric-api:0.84.0+1.20.1")
+    modImplementation("net.fabricmc:fabric-language-kotlin:1.10.0+kotlin.1.9.0")
 
     modApi("com.terraformersmc:modmenu:7.1.0")
     modApi("me.shedaniel.cloth:cloth-config-fabric:11.0.99") {
