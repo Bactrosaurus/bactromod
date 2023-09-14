15f1efc64fc705be39c955a936275a45c37368ed
Bactrosaurus <aubererdaniel@tutanota.de>
2023-09-14 02:08:27 +0200

    test

--- src/main/java/de/daniel/bactromod/mixins/hooks/settingsbutton/MixinTitleScreen.java
+++ src/main/java/de/daniel/bactromod/mixins/hooks/settingsbutton/MixinTitleScreen.java
@@ -14,6 +14,7 @@ import org.spongepowered.asm.mixin.injection.ModifyArg;
 @Mixin(TitleScreen.class)
 public class MixinTitleScreen extends Screen {
 
+
     protected MixinTitleScreen(Component component) {
         super(component);
     }
