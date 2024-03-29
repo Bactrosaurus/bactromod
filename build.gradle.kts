dfc21c28dc2070b7416e3a372763ed718a349872
Bactrosaurus <aubererdaniel@tutanota.de>
2024-03-29 20:25:29 +0100

    Updated Gradle to 8.7

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,11 @@
 plugins {
-    kotlin("jvm") version "1.9.22"
-    kotlin("plugin.serialization") version "1.9.22"
-    id("fabric-loom") version "1.5-SNAPSHOT"
+    kotlin("jvm") version "1.9.23"
+    kotlin("plugin.serialization") version "1.9.23"
+    id("fabric-loom") version "1.6-SNAPSHOT"
 }
 
 group = "de.daniel"
-version = "2.0"
+version = "2.1"
 
 repositories {
     mavenCentral()
@@ -13,14 +13,14 @@ repositories {
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.2")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.3")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.0")
 
     minecraft("com.mojang:minecraft:1.20.4")
     mappings(loom.officialMojangMappings())
 
-    modImplementation("net.fabricmc:fabric-loader:0.15.6")
-    modImplementation("net.fabricmc:fabric-language-kotlin:1.10.17+kotlin.1.9.22")
+    modImplementation("net.fabricmc:fabric-loader:0.15.7")
+    modImplementation("net.fabricmc:fabric-language-kotlin:1.10.19+kotlin.1.9.23")
 
     modApi("com.terraformersmc:modmenu:9.0.0")
 }
