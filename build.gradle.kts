b9b630f37053137544eec4e4160f7fdd4579b1b4
Daniel Auberer <daniel.auberer@uni-ulm.de>
2024-07-21 01:51:15 +0200

    Updated dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,11 @@
 plugins {
-    kotlin("jvm") version "1.9.23"
-    kotlin("plugin.serialization") version "1.9.23"
-    id("fabric-loom") version "1.6-SNAPSHOT"
+    kotlin("jvm") version "2.0.0"
+    id("fabric-loom") version "1.7-SNAPSHOT"
+    kotlin("plugin.serialization") version "2.0.0"
 }
 
 group = "de.daniel"
-version = "2.2"
+version = "2.3"
 
 repositories {
     mavenCentral()
@@ -13,16 +13,17 @@ repositories {
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.3")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.1")
 
-    minecraft("com.mojang:minecraft:1.20.6")
+    minecraft("com.mojang:minecraft:1.21")
     mappings(loom.officialMojangMappings())
 
     modImplementation("net.fabricmc:fabric-loader:0.15.11")
-    modImplementation("net.fabricmc:fabric-language-kotlin:1.10.19+kotlin.1.9.23")
+    modImplementation("net.fabricmc.fabric-api:fabric-api:0.100.3+1.21")
+    modImplementation("net.fabricmc:fabric-language-kotlin:1.11.0+kotlin.2.0.0")
 
-    modApi("com.terraformersmc:modmenu:10.0.0-beta.1")
+    modApi("com.terraformersmc:modmenu:11.0.0")
 }
 
 loom {
--- src/main/kotlin/de/daniel/bactromod/config/ConfigScreen.kt
+++ src/main/kotlin/de/daniel/bactromod/config/ConfigScreen.kt
@@ -5,122 +5,108 @@ import de.daniel.bactromod.windowborder.DwmApi
 import net.minecraft.client.Minecraft
 import net.minecraft.client.OptionInstance
 import net.minecraft.client.Options
-import net.minecraft.client.gui.components.Button
 import net.minecraft.client.gui.screens.Screen
-import net.minecraft.client.gui.screens.SimpleOptionsSubScreen
-import net.minecraft.network.chat.CommonComponents
+import net.minecraft.client.gui.screens.options.OptionsSubScreen
 import net.minecraft.network.chat.Component
 
-class ConfigScreen(screen: Screen) : SimpleOptionsSubScreen(
-    screen, Minecraft.getInstance().options, Component.translatable("bactromod.options.title"), options()
+class ConfigScreen(screen: Screen) : OptionsSubScreen(
+    screen, Minecraft.getInstance().options, Component.translatable("bactromod.options.title")
 ) {
 
-    companion object {
-
-        private fun options(): Array<OptionInstance<*>> {
-            return arrayOf(
-                OptionInstance(
+    override fun addOptions() {
+        this.list?.addSmall(
+            OptionInstance(
                 "bactromod.options.gamma",
                 OptionInstance.cachedConstantTooltip(Component.translatable("bactromod.options.gamma.description")),
                 Options::genericValueLabel,
                 OptionInstance.IntRange(0, 15),
                 Config.load().gammaMultiplier
-                ) { Config.save(Config.load().copy(gammaMultiplier = it)) },
+            ) { Config.save(Config.load().copy(gammaMultiplier = it)) },
 
-                OptionInstance.createBoolean(
-                    "bactromod.options.pumpkinblur",
-                    OptionInstance.cachedConstantTooltip(Component.translatable("bactromod.options.pumpkinblur.description")),
-                    Config.load().pumpkinBlur
-                ) { Config.save(Config.load().copy(pumpkinBlur = it)) },
+            OptionInstance.createBoolean(
+                "bactromod.options.pumpkinblur",
+                OptionInstance.cachedConstantTooltip(Component.translatable("bactromod.options.pumpkinblur.description")),
+                Config.load().pumpkinBlur
+            ) { Config.save(Config.load().copy(pumpkinBlur = it)) },
 
-                OptionInstance(
-                    "bactromod.options.fireoffset",
-                    OptionInstance.cachedConstantTooltip(Component.translatable("bactromod.options.fireoffset.description")),
-                    Options::genericValueLabel,
-                    OptionInstance.IntRange(-100, 100),
-                    Config.load().fireOffset
-                ) { Config.save(Config.load().copy(fireOffset = it)) },
+            OptionInstance(
+                "bactromod.options.fireoffset",
+                OptionInstance.cachedConstantTooltip(Component.translatable("bactromod.options.fireoffset.description")),
+                Options::genericValueLabel,
+                OptionInstance.IntRange(-100, 100),
+                Config.load().fireOffset
+            ) { Config.save(Config.load().copy(fireOffset = it)) },
 
-                OptionInstance(
-                    "bactromod.options.shieldoffset",
-                    OptionInstance.cachedConstantTooltip(Component.translatable("bactromod.options.shieldoffset.description")),
-                    Options::genericValueLabel,
-                    OptionInstance.IntRange(-100, 100),
-                    Config.load().shieldOffset
-                ) { Config.save(Config.load().copy(shieldOffset = it)) },
+            OptionInstance(
+                "bactromod.options.shieldoffset",
+                OptionInstance.cachedConstantTooltip(Component.translatable("bactromod.options.shieldoffset.description")),
+                Options::genericValueLabel,
+                OptionInstance.IntRange(-100, 100),
+                Config.load().shieldOffset
+            ) { Config.save(Config.load().copy(shieldOffset = it)) },
 
-                OptionInstance.createBoolean(
-                    "bactromod.options.boatmap",
-                    OptionInstance.cachedConstantTooltip(Component.translatable("bactromod.options.boatmap.description")),
-                    Config.load().showMapWhileInBoat
-                ) { Config.save(Config.load().copy(showMapWhileInBoat = it)) },
+            OptionInstance.createBoolean(
+                "bactromod.options.boatmap",
+                OptionInstance.cachedConstantTooltip(Component.translatable("bactromod.options.boatmap.description")),
+                Config.load().showMapWhileInBoat
+            ) { Config.save(Config.load().copy(showMapWhileInBoat = it)) },
 
-                OptionInstance.createBoolean(
-                    "bactromod.options.lavafog",
-                    OptionInstance.cachedConstantTooltip(Component.translatable("bactromod.options.lavafog.description")),
-                    Config.load().lavaFog
-                ) { Config.save(Config.load().copy(lavaFog = it)) },
+            OptionInstance.createBoolean(
+                "bactromod.options.lavafog",
+                OptionInstance.cachedConstantTooltip(Component.translatable("bactromod.options.lavafog.description")),
+                Config.load().lavaFog
+            ) { Config.save(Config.load().copy(lavaFog = it)) },
 
-                OptionInstance.createBoolean(
-                    "bactromod.options.snowfog",
-                    OptionInstance.cachedConstantTooltip(Component.translatable("bactromod.options.snowfog.description")),
-                    Config.load().powderSnowFog
-                ) { Config.save(Config.load().copy(powderSnowFog = it)) },
+            OptionInstance.createBoolean(
+                "bactromod.options.snowfog",
+                OptionInstance.cachedConstantTooltip(Component.translatable("bactromod.options.snowfog.description")),
+                Config.load().powderSnowFog
+            ) { Config.save(Config.load().copy(powderSnowFog = it)) },
 
-                OptionInstance.createBoolean(
-                    "bactromod.options.blindnessfog",
-                    OptionInstance.cachedConstantTooltip(Component.translatable("bactromod.options.blindnessfog.description")),
-                    Config.load().blindnessFog
-                ) { Config.save(Config.load().copy(blindnessFog = it)) },
+            OptionInstance.createBoolean(
+                "bactromod.options.blindnessfog",
+                OptionInstance.cachedConstantTooltip(Component.translatable("bactromod.options.blindnessfog.description")),
+                Config.load().blindnessFog
+            ) { Config.save(Config.load().copy(blindnessFog = it)) },
 
-                OptionInstance.createBoolean(
-                    "bactromod.options.darknessfog",
-                    OptionInstance.cachedConstantTooltip(Component.translatable("bactromod.options.darknessfog.description")),
-                    Config.load().darknessFog
-                ) { Config.save(Config.load().copy(darknessFog = it)) },
+            OptionInstance.createBoolean(
+                "bactromod.options.darknessfog",
+                OptionInstance.cachedConstantTooltip(Component.translatable("bactromod.options.darknessfog.description")),
+                Config.load().darknessFog
+            ) { Config.save(Config.load().copy(darknessFog = it)) },
 
-                OptionInstance.createBoolean(
-                    "bactromod.options.waterfog",
-                    OptionInstance.cachedConstantTooltip(Component.translatable("bactromod.options.waterfog.description")),
-                    Config.load().waterFog
-                ) { Config.save(Config.load().copy(waterFog = it)) },
+            OptionInstance.createBoolean(
+                "bactromod.options.waterfog",
+                OptionInstance.cachedConstantTooltip(Component.translatable("bactromod.options.waterfog.description")),
+                Config.load().waterFog
+            ) { Config.save(Config.load().copy(waterFog = it)) },
 
-                OptionInstance.createBoolean(
-                    "bactromod.options.skyfog",
-                    OptionInstance.cachedConstantTooltip(Component.translatable("bactromod.options.skyfog.description")),
-                    Config.load().skyFog
-                ) { Config.save(Config.load().copy(skyFog = it)) },
+            OptionInstance.createBoolean(
+                "bactromod.options.skyfog",
+                OptionInstance.cachedConstantTooltip(Component.translatable("bactromod.options.skyfog.description")),
+                Config.load().skyFog
+            ) { Config.save(Config.load().copy(skyFog = it)) },
 
-                OptionInstance.createBoolean(
-                    "bactromod.options.thickfog",
-                    OptionInstance.cachedConstantTooltip(Component.translatable("bactromod.options.thickfog.description")),
-                    Config.load().thickFog
-                ) { Config.save(Config.load().copy(thickFog = it)) },
+            OptionInstance.createBoolean(
+                "bactromod.options.thickfog",
+                OptionInstance.cachedConstantTooltip(Component.translatable("bactromod.options.thickfog.description")),
+                Config.load().thickFog
+            ) { Config.save(Config.load().copy(thickFog = it)) },
 
-                OptionInstance.createBoolean(
-                    "bactromod.options.terrainfog",
-                    OptionInstance.cachedConstantTooltip(Component.translatable("bactromod.options.terrainfog.description")),
-                    Config.load().terrainFog
-                ) { Config.save(Config.load().copy(terrainFog = it)) },
+            OptionInstance.createBoolean(
+                "bactromod.options.terrainfog",
+                OptionInstance.cachedConstantTooltip(Component.translatable("bactromod.options.terrainfog.description")),
+                Config.load().terrainFog
+            ) { Config.save(Config.load().copy(terrainFog = it)) },
 
-                OptionInstance.createBoolean(
-                    "bactromod.options.windowborders",
-                    OptionInstance.cachedConstantTooltip(Component.translatable("bactromod.options.windowborders.description")),
-                    Config.load().darkWindowBorders
-                ) {
-                    Config.save(Config.load().copy(darkWindowBorders = it))
-                    if (SystemInfo.systemIsWindows11) DwmApi.updateDwm(Minecraft.getInstance().window.window)
-                }
-            )
-        }
-
-    }
-
-    override fun addFooter() {
-        addRenderableWidget(
-            Button.builder(
-                CommonComponents.GUI_DONE
-            ) { minecraft!!.setScreen(lastScreen) }.bounds(width / 2 - 75, height - 27, 150, 20).build()
+            OptionInstance.createBoolean(
+                "bactromod.options.windowborders",
+                OptionInstance.cachedConstantTooltip(Component.translatable("bactromod.options.windowborders.description")),
+                Config.load().darkWindowBorders
+            ) {
+                Config.save(Config.load().copy(darkWindowBorders = it))
+                if (SystemInfo.systemIsWindows11) DwmApi.updateDwm(Minecraft.getInstance().window.window)
+            }
         )
     }
 
