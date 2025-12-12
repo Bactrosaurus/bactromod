0bc7c928cb4638531adbb352668edb8e7b4fb4d4
Bactrosaurus <aubererdaniel@tutanota.de>
2025-12-12 18:52:50 +0100

    Updated to Minecraft 1.21.11

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,9 @@
 plugins {
-    id("fabric-loom") version "1.11-SNAPSHOT"
+    id("fabric-loom") version "1.14-SNAPSHOT"
 }
 
 group = "de.daniel"
-version = "3.3"
+version = "3.4"
 
 repositories {
     mavenCentral()
@@ -11,10 +11,10 @@ repositories {
 }
 
 dependencies {
-    minecraft("com.mojang:minecraft:1.21.10")
-    mappings("net.fabricmc:yarn:1.21.10+build.1")
-    modImplementation("net.fabricmc:fabric-loader:0.17.2")
-    modApi("com.terraformersmc:modmenu:16.0.0-rc.1")
+    minecraft("com.mojang:minecraft:1.21.11")
+    mappings("net.fabricmc:yarn:1.21.11+build.3")
+    modImplementation("net.fabricmc:fabric-loader:0.18.2")
+    modApi("com.terraformersmc:modmenu:17.0.0-alpha.1")
 }
 
 loom {
